a7ba870766de2d1a460bcbd5ce0a0ea6a441a6f4
Demonation34 <lyalyavka@gmail.com>
2024-10-12 03:02:44 +0400

    Auto commit: изменения в index.html

---
+++ auto_commit.ps1
@@ -0,0 +1,8 @@
+# Сначала устанавливаем параметры для работы с PowerShell
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Мониторим изменения в файле index.html и автоматически выполняем команды
+chokidar "F:\Proect\Proect\index.html" -c `
+  "git add .`
+   git commit -m 'Auto commit: изменения в index.html' `
+   git push origin main"
